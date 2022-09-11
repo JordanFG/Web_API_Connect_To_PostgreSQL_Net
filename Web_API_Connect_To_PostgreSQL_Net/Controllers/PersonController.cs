7c47a8d010416e4389d31a345eb4b4525f422f91
fred <fredjordan288@gmail.com>
2022-09-11 23:19:41 +0100

    Add PersonController and Get, Get{Id}, Post, Put, Delete EndPoint

--- Web_API_Connect_To_PostgreSQL_Net/Controllers/PersonController.cs
+++ Web_API_Connect_To_PostgreSQL_Net/Controllers/PersonController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Web_API_Connect_To_PostgreSQL_Net.Datas;
+using Web_API_Connect_To_PostgreSQL_Net.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,36 +13,82 @@ namespace Web_API_Connect_To_PostgreSQL_Net.Controllers
     [Route("api/[controller]")]
     public class PersonController : Controller
     {
-        // GET: api/values
+
+        private readonly DataContext context;
+
+        public PersonController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Person
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetAllPerson()
         {
-            return new string[] { "value1", "value2" };
+            List<Person> people = await this.context.people.ToListAsync();
+            return Ok(people);
         }
 
-        // GET api/values/5
+        // GET api/Person/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            return "value";
+            var person = await this.context.people.FindAsync(id);
+            if (person == null)
+            {
+                return BadRequest("person doesn't exist");
+            }
+            return Ok(person);
         }
 
-        // POST api/values
+        // POST api/Person
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post(Person person)
         {
+            this.context.people.Add(person);
+            await this.context.SaveChangesAsync();
+            List<Person> people = await this.context.people.ToListAsync();
+            return Ok(people);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // PUT api/Person/5
+        [HttpPut]
+        public async Task<IActionResult> Put(Person person)
         {
+            var updatePerson = await this.context.people.FindAsync(person.Id);
+
+            if (updatePerson == null)
+            {
+                return BadRequest("person doesn't exist");
+            }
+
+            updatePerson.FirstName = person.FirstName;
+            updatePerson.LastName = person.LastName;
+            updatePerson.Place = person.Place;
+
+            await this.context.SaveChangesAsync();
+
+            List<Person> people = await this.context.people.ToListAsync();
+            return Ok(people);
+
         }
 
-        // DELETE api/values/5
+        // DELETE api/Person/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            var person = await this.context.people.FindAsync(id);
+            if (person == null)
+            {
+                return BadRequest("person doesn't exist");
+            }
+
+            this.context.people.Remove(person);
+            await this.context.SaveChangesAsync();
+
+            List<Person> people = await this.context.people.ToListAsync();
+            return Ok(people);
+
         }
     }
 }
---
+++ Web_API_Connect_To_PostgreSQL_Net/Migrations/20220911215741_CreateInitial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Web_API_Connect_To_PostgreSQL_Net.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "people",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Place = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_people", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "people");
+        }
+    }
+}
